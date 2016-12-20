7bf186fb5e0fa2d5c4d430a7525a9aeadbb06f61
Dhruv Ramdev <pythonbrilliant@gmail.com>
2016-12-20 18:30:10 +0530

    Day-6 Added

---
+++ Day-6/part-2.py
@@ -0,0 +1,17 @@
+file = open("input.txt" , "r")
+li = []
+code = ""
+lines = file.readlines()
+for i in range(8):
+    di = {}
+    li.append([line[i] for line in lines])
+    for j in li[i] :
+        if j in di :
+            di[j] += 1
+        else :
+            di[j] = 1
+    #print di
+    #print 
+    code += min(di , key= di.get)    
+
+print code
